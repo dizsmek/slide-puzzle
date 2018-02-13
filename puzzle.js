b2ec89fe0beeec2c494e0308e5f2affd38501a41
Misu Ballo <dizsmek@gmail.com>
2018-02-13 21:35:07 +0100

    The demo works!

--- puzzle.js
+++ puzzle.js
@@ -22,35 +22,13 @@ const dataTable = {
 	"P" : ["L", "O"]
 }
 
-// let images = document.getElementsByTagName('IMG');
-// let black = function () {
-// 	for (let index = 0; index < images.length; index++) {
-// 		if(images[index].src.endsWith("black.jpeg")) {
-// 			return images[index].id;
-// 		}
-// 	}
-// };
-//
-// console.log(black());
-//
-// function slide(event) {
-// 	event = event || window.event;
-// console.log(event.target.id); //event.target is not working in Firefox
-//
-// 	for (let key in dataTable) {
-// 		if (event.target.id === key) {
-// 			for(let i = 0; i < dataTable[key].length; i++) {
-// 				if(dataTable[key][i] === black()) {
-// 					console.log('test');
-// 				}
-// 			}
-// 		}
-// 	}
-// }
-
 function slide(clickedImage) {
-	console.log(clickedImage);
+console.log(clickedImage);
 	dataTable[clickedImage.id].forEach(function(id) {
-  	console.log(id);
+console.log(document.getElementById(id).src);
+  	if(document.getElementById(id).src.endsWith("black.jpg")) {
+			document.getElementById(id).src = clickedImage.src;
+			clickedImage.src = "images/black.jpg";
+		}
 	});
 }
